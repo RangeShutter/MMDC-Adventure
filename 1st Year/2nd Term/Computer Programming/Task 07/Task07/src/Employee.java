f5498ff024592fe6fa0506b1a4e035b8b4d88600
Colin Bactong <lukkiebactong@gmail.com>
2025-03-11 17:34:37 +0800

    Add files via upload

---
+++ 1st Year/2nd Term/Computer Programming/Task 07/Task07/src/Employee.java
@@ -0,0 +1,8 @@
+public class Employee {
+    private int employeeNumber;
+    private String lastName;
+    private String firstName;
+    private String birthday;
+    private String address;
+    private String 
+}
