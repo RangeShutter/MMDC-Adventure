12a873092a6aa2778c6f44b7b691cc9219eba379
Colin Bactong <lukkiebactong@gmail.com>
2025-03-11 17:35:32 +0800

    Delete 1st Year/2nd Term/Computer Programming/Task 07/Task07 directory

--- 1st Year/2nd Term/Computer Programming/Task 07/Task07/src/Employee.java
+++
@@ -1,8 +0,0 @@
-public class Employee {
-    private int employeeNumber;
-    private String lastName;
-    private String firstName;
-    private String birthday;
-    private String address;
-    private String 
-}
